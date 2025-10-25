29ba4b88f771d8f4badc0368f9eaf3b9a820eee9
Skyzo-be <73526678+Skyzo-be@users.noreply.github.com>
2025-10-25 09:39:34 +0200

    Add French localization for HandyNotes HallowsEnd

---
+++ HandyNotes_HallowsEnd/Locale-frFR.lua
@@ -0,0 +1,23 @@
+-- HandyNotes_HallowsEnd
+local L = LibStub("AceLocale-3.0"):NewLocale("HandyNotes_HallowsEnd", "frFR")
+
+if not L then return end
+
+-- General
+L["HandyNotes - HallowsEnd"] = "HandyNotes - La Sanssaint"
+L["HallowsEnd"] = "La Sanssaint"
+L["Hallow's End Candy Bucket locations"] = "Emplacements des seaux à bonbons de la Sanssaint."
+L["Create waypoint"] = "Créer un point de repère"
+L["Close"] = "Fermer"
+
+-- Options
+L["These settings control the look and feel of the icon"] = "Ces paramètres contrôlent l'apparence des icônes de la Sanssaint"
+
+L["Icon Scale"] = "Échelle des icônes"
+L["The scale of the icons"] = "Modifie l'échelle des icônes"
+L["Icon Alpha"] = "Transparence des icônes"
+L["The alpha transparency of the icons"] = "Modifie la transparence des icônes"
+L["Show icons for bonfires you have already visited"] = "Afficher les icônes des seaux de bonbons que vous avez déjà visités"
+L["Show completed"] = "Afficher les quêtes terminées"
+L["Show only during the Summer Festival"] = "Afficher uniquement pendant la Sanssaint"
+L["Display bonfires only during the Fire Festival period"] = "Afficher les seaux de bonbons uniquement pendant la période de la Sanssaint."
